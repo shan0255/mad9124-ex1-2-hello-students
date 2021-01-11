08e9e69c99bc409c5ea0d5fc6e4da101f6a2d61d
Eric Shantz <shan0255@algonquinlive.com>
2021-01-11 13:19:55 -0500

    complete ex1-2

--- helloStudents.js
+++ helloStudents.js
@@ -1,9 +1,13 @@
 'use strict'
 
 // 1. Read the JSON file into a variable called students
+const students = require('./students.json');
 
 // 2. Iterate over the students array and print Hello with their full names to the console
-// e.g. Hello Walter Baker
+students.forEach( ({firstName , lastName}) => {
+    console.log(`Hello ${firstName} ${lastName}`);
+});
 
 // 3. Print out the number of last names starting with the letter D
-// e.g. Count of last names starting with D is 1
+let filt = students.filter(({lastName}) => lastName.startsWith('D'));
+console.log(`Count of last names starting with "D" is ${filt.length}`);
